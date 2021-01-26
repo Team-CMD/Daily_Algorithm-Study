e8ed76cc0d1ddc1a609af217f87ae34d0aa8e73d
CHO_MJ <62231893+cood5199@users.noreply.github.com>
2021-01-26 16:10:47 +0900

    Add files via upload

--- #01. 단계별 문제풀이/5. List/B#1546.py
+++ #01. 단계별 문제풀이/5. List/B#1546.py
@@ -1,9 +1,29 @@
-num = int(input())
-arr = list(map(int, input().split()))
-New_arr = [0]*num
+#재훈
+def calc_mean(n, m, s):
+    re_s = [0]*n
+    tmp = 0
+    for i in range(n):
+        re_s[i] = s[i]/m*100
+        tmp += re_s[i]
+    result = tmp/n
+    print(result)
 
-for i in range (0,num):
-    New_arr[i] = (arr[i] * 100 )/max(arr)
+if __name__ == '__main__':
+    N = int(input())
+    score = list(map(int, input().split()))
+    M = max(score)
+    calc_mean(N, M, score)
 
-print(sum(New_arr)/num)
+'''
+#민준
+N = int(input())
+Before_score = list(map(int, input().split()))
+tmp = (sum(Before_score)/N)/max(Before_score)*100
+print(tmp)
 
+'''
+
+    
+
+    
+    
