8492289b2b5f6714b500b3976e6d939b13b7fe3e
JH9892 <jhchoi09@naver.com>
2021-01-29 16:07:14 +0900

    Add to contents 6.함수, 7.문자열

---
+++ #01. 단계별 문제풀이/7. 문자열/B#11720.py
@@ -0,0 +1,22 @@
+N = int(input())
+n = list(input())
+a = 0
+A = []
+
+for i in range(N):
+    A.append(int(n[i]))
+    a = a + A[i]
+
+print(a)
+
+=================================
+A = [] # A라는 list만 생성.
+B = [1,2,3,4,5] # B라는 list를 생성하고 값도 초기화
+
+
+
+
+
+
+
+
